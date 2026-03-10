167f623393bf362ceb4c98236db6afbe7425a64b
Sidy-dgn06 <sidydiagne07@gmail.com>
2026-03-10 08:32:39 +0000

    Delete EvasStore.js

--- EvasStore.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.ajouter-panier').forEach(button => {
-    button.addEventListener('click', function() {
-        const produitId = this.parentElement.getAttribute('data-id');
-        alert('Produit ajouté au panier : ' + produitId);
-        // Logique d'ajout au panier à implémenter ici
-    });
-});
